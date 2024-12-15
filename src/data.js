f0990639069a2d61c650c4f9ef5ff5fe2ba7b0ba
theguy-14 <sandeepraj022003@gmail.com>
2024-12-15 17:09:03 +0530

    update

--- src/data.js
+++ src/data.js
@@ -9,7 +9,7 @@ function music_data() {
             id: uuidv4(),
             active: true,
             color: ["#ABC0D1","#CFCBBF"],
-            audio: "https://pagalworld.ink/files/download/id/9851"
+            audio: "https://s3.eu-central-1.wasabisys.com/audio.com.audio/transcoding/77/76/1818505113157677-1818505113266893-1818505120123902.mp3?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=W7IA3NSYSOQIKLY9DEVC%2F20241215%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20241215T112039Z&X-Amz-SignedHeaders=host&X-Amz-Expires=518400&X-Amz-Signature=85257b1eeeda972bc9faee1579a7515d0dbf309cafe624489015552344e95303"
         },
         {
             name: "Dil Mere",
@@ -27,16 +27,16 @@ function music_data() {
             id: uuidv4(),
             active: false,
             color: ["#8E938F","#5F7372"],
-            audio: "https://pagalfree.com/musics/128-Jee Le Zaraa - Talaash 128 Kbps.mp3"
+            audio: "https://s3.eu-central-1.wasabisys.com/audio.com.audio/transcoding/57/58/1818503819285857-1818503819350984-1818504038647002.mp3?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=W7IA3NSYSOQIKLY9DEVC%2F20241215%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20241215T110452Z&X-Amz-SignedHeaders=host&X-Amz-Expires=518400&X-Amz-Signature=626396706244aafb95c34cbfea348fd6a4e3f2c7a527ff76c989f65fec47aa1c"
         },
         {
             name: "Saajna",
             artist: "Mitraz",
-            cover: "https://pagalfree.com/images/128Saajna%20-%20Mitraz%20128%20Kbps.jpg",
+            cover: "https://lh3.googleusercontent.com/SIi8j596tpxQ8xkCL6KafbqRlz99RhUzjtkhX568m4SRUwWLSjuJEnCel-iy5eL6Q2yXY_pRPsR1R0E=w544-h544-l90-rj",
             id: uuidv4(),
             active: false,
             color: ["#999D8E","#2D4543"],
-            audio: "https://pagalfree.com/musics/128-Saajna - Mitraz 128 Kbps.mp3"
+            audio: "https://s3.eu-central-1.wasabisys.com/audio.com.audio/transcoding/25/38/1818505138073825-1818505138134514-1818505142775937.mp3?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=W7IA3NSYSOQIKLY9DEVC%2F20241215%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20241215T112145Z&X-Amz-SignedHeaders=host&X-Amz-Expires=518400&X-Amz-Signature=fb09e68f86cb1092e534115f173a9e171532a8bca0ec7dba26f8dc7b95f60098"
         },
         {
             name: "Nadaaniyan",
@@ -45,7 +45,7 @@ function music_data() {
             id: uuidv4(),
             active: false,
             color: ["#A96D65","#7F7069"],
-            audio: "https://www.pagalworld.com.so/files/download/type/128/id/22091"
+            audio: "https://s3.eu-central-1.wasabisys.com/audio.com.audio/transcoding/64/55/1818505083615564-1818505083656334-1818505090618467.mp3?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=W7IA3NSYSOQIKLY9DEVC%2F20241215%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20241215T112012Z&X-Amz-SignedHeaders=host&X-Amz-Expires=518400&X-Amz-Signature=1ea1128d6d749305aef0b66633e31f31aac0c58a7f0e7d17fb44c2cc1d2a70a1"
         },
         {
             name: "Ek Raat",
@@ -54,17 +54,26 @@ function music_data() {
             id: uuidv4(),
             active: false,
             color: ["#7D4945","#1A0E18"],
-            audio: "https://pagalsongs.com.in/files/download?id=10084"
+            audio: "https://s3.eu-central-1.wasabisys.com/audio.com.audio/transcoding/06/80/1818505095178006-1818505095211645-1818505103609412.mp3?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=W7IA3NSYSOQIKLY9DEVC%2F20241215%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20241215T112028Z&X-Amz-SignedHeaders=host&X-Amz-Expires=518400&X-Amz-Signature=ba320114d49b6dc1a8b90cbde08a576295df98dd77582cdec68843e4dddfd033"
         },
         {
             name: "Samjho Na",
             artist: "Aditya Rikhari",
-            cover: "https://www.pagalworld.com.so/uploads/thumb/sft31/15440_4.jpg",
+            cover: "https://lh3.googleusercontent.com/VtXq1g11gvZ3TYfzul7RimkulFWisPARNMW0UeNa0r6x6-zzKtcRo9Sh9hG-2U0Ujw6EKs5SnasE_OIT=w544-h544-l90-rj",
             id: uuidv4(),
             active: false,
             color: ["#958D9A","#685597"],
-            audio: "https://www.pagalworld.com.so/files/download/type/128/id/15440"
-        }
+            audio: "https://s3.eu-central-1.wasabisys.com/audio.com.audio/transcoding/01/53/1818505062015301-1818505062122718-1818505072084602.mp3?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=W7IA3NSYSOQIKLY9DEVC%2F20241215%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20241215T112001Z&X-Amz-SignedHeaders=host&X-Amz-Expires=518400&X-Amz-Signature=0ffec964aef6fc70f40dbbe15c617d5ec4fea7cd7f2ffd2acced2964ff536449"
+        },
+        {
+            name: "Nadiyon Sa",
+            artist: "Mitraz",
+            cover: "https://lh3.googleusercontent.com/loVG90ri6u3FUvmjRfpaFmJbgLtqJAUpmOcQDsykBW7gDa3Zy_3Z3rOzq5zh4uCpntzaaRoE9PCwJDut=w544-h544-l90-rj",
+            id: uuidv4(),
+            active: false,
+            color: ["#6C6B35","#C19F7C"],
+            audio: "https://s3.eu-central-1.wasabisys.com/audio.com.audio/transcoding/52/64/1818505126986452-1818505127072643-1818505131575625.mp3?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=W7IA3NSYSOQIKLY9DEVC%2F20241215%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20241215T112050Z&X-Amz-SignedHeaders=host&X-Amz-Expires=518400&X-Amz-Signature=9c48ecb857fb2ed7096d6b09e1e5d8475b73460d8c98d63f05e983a0d7d06d77"
+        },
     ];
 }
 
